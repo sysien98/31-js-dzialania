5555a4547b3da26c432f11852bf7eb7619b6568c
Sylwia Sadowska <sylwiaxsadowska@gmail.com>
2018-10-15 16:37:00 +0200

    dzialania

---
+++ main.js
@@ -0,0 +1,19 @@
+function wynik(x, y) {
+       if (x+y > 0) {
+    console.log('Wynik dodawania wynosi ' + (x + y));
+       } else {
+           console.log('Wynik jest ujemny');
+       }
+    if (x-y > 0) {
+        console.log('Wynik odejmowania wynosi ' + (x - y));
+    } else {
+           console.log('Wynik jest ujemny');
+       }
+    if (x*y >0) {
+        console.log('Wynik mnożenia wynosi ' + (x * y));
+    } else {
+           console.log('Wynik jest ujemny');
+       }
+        
+}
+wynik(2, 3);
